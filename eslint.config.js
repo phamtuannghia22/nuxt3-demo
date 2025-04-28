edec4fb8d19d472d2f39511fff308abce2dfd662
nghiapt <@Dmin4123>
2025-04-28 17:08:51 +0700

    init project

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,vue}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+  ...pluginVue.configs["flat/strongly-recommended"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
   {
     ignores: [
