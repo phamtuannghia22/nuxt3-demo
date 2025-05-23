f0474cff6dfe12840a0366bfdc7501fcc4bf266c
nghiapt <@Dmin4123>
2025-05-23 17:34:42 +0700

    init project

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig([
         ...globals.node,
         defineNuxtConfig: "readonly",
         useRuntimeConfig: "readonly",
+        definePageMeta: "readonly",
       },
     },
   },
